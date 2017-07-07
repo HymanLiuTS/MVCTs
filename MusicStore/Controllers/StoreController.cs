fd8eaeaa9e975e9b28c31de9364830b4a0872f8e
刘志辉 <879651072@qq.com>
2017-07-07 18:52:21 +0800

    第二章 控制器

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public String Index()
+        {
+            return "Hello from Stor()";
+        }
+
+        public String Browse(string genre)
+        {
+            return HttpUtility.HtmlEncode( "传过来的参数是" + genre);
+        }
+
+        // GET: /Store/Details/id
+        public String Details(int id)
+        {
+            return "Store.Details(),id=" + id ;
+        }
+    }
+}
