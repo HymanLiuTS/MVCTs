3dc9536b5b92f9c805ea4081811ba9cc57908581
刘志辉 <879651072@qq.com>
2017-07-12 19:35:04 +0800

    4.1 模型和EF

--- MusicStore/Models/Album.cs
+++ MusicStore/Models/Album.cs
@@ -7,6 +7,30 @@ namespace MusicStore.Models
 {
     public class Album
     {
-        public string Title { get; set; }
+        public virtual int AlbumId { get; set; }
+        public virtual int GenreId { get; set; }
+        public virtual int ArtistId { get; set; }
+        public virtual String Title { get; set; }
+        public virtual decimal Price { get; set; }
+
+        public virtual String Description { get; set; }
+        public virtual String AlbumUrl { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
+
+    }
+
+    public class Artist
+    {
+        public virtual int ArtistId { get; set; }
+        public virtual String Name  { get; set; }
+    }
+
+    public class Genre
+    {
+        public virtual int GenreId { get; set; }
+        public virtual String Name { get; set; }
+        public virtual String Description { get; set; }
+        public virtual List<Album> Albums { get; set; }
     }
 }
---
+++ MusicStore/Models/MusicStoreDBInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Models
+{
+    public class MusicStoreDBInitializer:System.Data.Entity.DropCreateDatabaseAlways<MusicStoreDB>
+    {
+        protected override void Seed(MusicStoreDB context)
+        {
+            context.Artists.Add(new Artist() { Name = "Hyman" });
+            context.Genres.Add(new Genre() { Name = "摇滚", Description = "无" });
+            context.Albums.Add(new Album()
+            {
+                Artist = new Artist { Name="Rush"},
+                Genre = new Genre { Name="Rook"},
+                Price=9.9m,
+                Title="Pitles"
+            });
+            base.Seed(context);
+        }
+    }
+}
